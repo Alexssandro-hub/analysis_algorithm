0da2daa8d271029c52a55c6d114ac42922527d63
Alexssandro Rodrigues Tabosa <71848388+Alexssandro-hub@users.noreply.github.com>
2022-10-18 23:55:43 -0300

    Ajustes nas iterações e correção

--- BinaryInsertionAndInsertionSort.cpp
+++ BinaryInsertionAndInsertionSort.cpp
@@ -54,77 +54,50 @@ void insertionSort(float arr[], int n)
     }
 }
 
-void printArray(float arr[], int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-        cout << (float)arr[i] << " ";
-    cout << endl;
-}
-
 int main()
 {
     srand(time(NULL));
 
-    int n[10];
-    int m = rand()%(20-10+1)+10; //15
-
-    //arrayTam
-    for(int i = 0; i < 10; i++)
-        n[i] = 10 + (rand() % 10000);
+    int tam;
+    int m = rand()%(20-10+1)+10;
 
     clock_t startInsertion, endInsertion;
-
-    for(int i = 0; i < 10; i++){
-        startInsertion = clock();
-        cout << "Contagem Insercao Default " << "\n";
-        cout << " TAMANHO: " << m << "\n";
-        cout << " Iteracao: " << i << "\n";
-
-        float arr[n[i]];
-
-        for(int j = 0; j < m; j++)
-            arr[j] = (float) (rand() % ( (2 * n[i] - (-2*n[i]) + 1 ) )+(-2*n[i]));
-
-        int n = sizeof(arr) / sizeof(arr[0]);
-
-        insertionSort(arr, n);
-        //printArray(m, n);
-        endInsertion = clock();
-
-        cout << "Array[" << i <<"]" << " com o tempo: "<< "\n";
-        double time_taken = double(endInsertion - startInsertion) / double(CLOCKS_PER_SEC);
-        cout << "O tempo gasto pelo programa é : " << fixed
-             << time_taken << setprecision(5);
-        cout << " segundos " << endl;
-
-        cout << "\n";
-    }
-
     clock_t startInsertionBinary, endInsertionBinary;
 
-    for(int i = 0; i < 10; i++){
-        startInsertionBinary = clock();
-        cout << "Contagem Insercao Binaria " << "\n";
-        cout << " TAMANHO: " << m << "\n";
+    for(int i = 0; i < m; i++){
+        tam = 10 + (rand() % 10000);
+        cout << "Contagem Insercao Default " << "\n";
         cout << " Iteracao: " << i << "\n";
 
-        float arr[n[i]];
+        float arr[tam];
 
-        for(int j = 0; j < m; j++)
-            arr[j] = (float) (rand() % ( (2*n[i] - (-2*n[i]) + 1 ) )+(-2*n[i]));
+        for(int j = 0; j < tam; j++)
+            arr[j] = (float) (rand() % ( (2 * tam - (-2 * tam) + 1 ) )+(-2 * tam));
 
         int n = sizeof(arr) / sizeof(arr[0]);
+        cout << " TAMANHO: " << tam << "\n";
 
+        startInsertion = clock();
+        insertionSort(arr, n);
+        endInsertion = clock();
+
+        cout << "ArrayInsertion[" << i <<"]" << " com o tempo: "<< "\n";
+        double time_taken = double(endInsertion - startInsertion) / double(CLOCKS_PER_SEC);
+        cout << "O tempo gasto pelo programa é : " << fixed
+             << time_taken * 1000 << setprecision(5);
+        cout << " milisegundos(ms) " << endl;
+
+        cout << "\n";
+
+        startInsertionBinary = clock();
         insertionSortWithBinary(arr, n);
-        //printArray(m, n);
         endInsertionBinary = clock();
 
-        cout << "Array[" << i <<"]" << " com o tempo: "<< "\n";
-        double time_taken = double(endInsertionBinary - startInsertionBinary) / double(CLOCKS_PER_SEC);
+        cout << "ArrayBinaryInsertion[" << i <<"]" << " com o tempo: "<< "\n";
+        double time_taken_binary = double(endInsertionBinary - startInsertionBinary) / double(CLOCKS_PER_SEC);
         cout << "O tempo gasto pelo programa é : " << fixed
-             << time_taken << setprecision(5);
-        cout << " segundos " << endl;
+             << time_taken_binary * 1000 << setprecision(5);
+        cout << " milisegundos(ms) " << endl;
 
         cout << "\n";
     }
